92230528fede2be6b41bc033e67dab47272bdfb8
Yu Tanaka <githubam1@am1.jp>
2023-10-08 17:33:50 +0900

    氷床を実装

---
+++ Assets/AM1/VBirdHiyoko/Scripts/Block/IceGround.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AM1.VBirdHiyoko
+{
+    /// <summary>
+    /// 氷の床。上のオブジェクトが滑る。
+    /// </summary>
+    public class IceGround : MonoBehaviour, IBlockInfo
+    {
+        public bool IsSlippery => true;
+    }
+}
