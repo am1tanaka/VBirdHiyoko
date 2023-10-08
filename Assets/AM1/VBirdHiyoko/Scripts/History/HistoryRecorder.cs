2581acdd9ee703ceb51d4e1fb6fa3e3e1dce5fb2
Yu Tanaka <githubam1@am1.jp>
2023-10-08 19:29:39 +0900

    HistoryRecorderTest完了

--- Assets/AM1/VBirdHiyoko/Scripts/History/HistoryRecorder.cs
+++ Assets/AM1/VBirdHiyoko/Scripts/History/HistoryRecorder.cs
@@ -12,6 +12,16 @@ namespace AM1.VBirdHiyoko
     public static class HistoryRecorder
     {
         /// <summary>
+        /// カウンターをループさせるためのビット積値。
+        /// </summary>
+        public static int CounterAnd => 127;
+
+        /// <summary>
+        /// 履歴の記録上限。超過検出の都合でシーケンス番号より小さい値にする。
+        /// </summary>
+        static int HistoryDataMax => 100;
+
+        /// <summary>
         /// 履歴保存用クラス
         /// </summary>
         public static IHistorySaver historySaver;
@@ -98,7 +108,11 @@ namespace AM1.VBirdHiyoko
         /// <param name="behaviour">移動を開始したオブジェクト</param>
         public static void StartMove(HistoryBehaviour behaviour)
         {
-            Debug.Log("未実装");
+            if (!movingObjects.Contains(behaviour))
+            {
+                movingObjects.Add(behaviour);
+                behaviour.StartMove();
+            }
         }
 
         /// <summary>
@@ -106,7 +120,97 @@ namespace AM1.VBirdHiyoko
         /// </summary>
         public static void MoveDone()
         {
-            Debug.Log("未実装");
+            if (movingObjects.Count == 0) return;
+
+            Counter = (Counter + 1) & CounterAnd;
+            toLatestList.Clear();
+
+            HistoryData result = null;
+            for (int i = 0; i < movingObjects.Count; i++)
+            {
+                if (result == null)
+                {
+                    result = GetBlankHistoryData();
+                    result.step = Counter;
+                }
+
+                if (movingObjects[i].MoveDone(ref result))
+                {
+                    // 履歴を記録
+                    historyDataList.AddLast(result);
+                    result = null;
+                }
+            }
+
+            // 余ったらプールに戻す
+            if (result != null)
+            {
+                ReleaseHistoryDataToPool(result);
+            }
+
+            // 移動オブジェクトをクリア
+            movingObjects.Clear();
+
+            // 登録数オーバーの処理
+            RemoveOverData();
+
+            // 配列化
+            HistoryArray = null;
+            HistoryArray = historyDataList.ToArray<HistoryData>();
+        }
+
+        /// <summary>
+        /// データ登録数が規定数をオーバーしていたら、古いデータを削除する。
+        /// </summary>
+        static void RemoveOverData()
+        {
+            int d = historyDataList.Last.Value.step - historyDataList.First.Value.step + 1;
+            if (d < 0)
+            {
+                d += (CounterAnd + 1);
+            }
+            if (d <= HistoryDataMax) return;
+
+            // 対象のシーケンスIDまで削除する
+            // 最後が127の時、28-127を残す。よって、28が出たらループを停止したい
+            int targetStep = historyDataList.Last.Value.step - HistoryDataMax + 1;
+            if (targetStep < 0)
+            {
+                targetStep += (CounterAnd + 1);
+            }
+            while (historyDataList.First.Value.step != targetStep)
+            {
+                ReleaseHistoryDataToPool(historyDataList.First.Value);
+                historyDataList.RemoveFirst();
+            }
+        }
+
+        /// <summary>
+        /// 利用可能な履歴データを返す。
+        /// プールに余裕があればプールから取り出し、なければ新規作成する。
+        /// </summary>
+        /// <returns>プールから取り出したか、新規作成したHistoryDataのインスタンス</returns>
+        static HistoryData GetBlankHistoryData()
+        {
+            if (historyDataPool.Count > 0)
+            {
+                var data = historyDataPool.First.Value;
+                historyDataPool.RemoveFirst();
+                return data;
+            }
+            else
+            {
+                return new HistoryData();
+            }
+        }
+
+        /// <summary>
+        /// 指定の履歴データをプールに戻す。
+        /// </summary>
+        /// <param name="data">戻したい履歴データのインスタンス</param>
+        static void ReleaseHistoryDataToPool(HistoryData data)
+        {
+            historyDataPool.AddLast(data);
         }
 
         /// <summary>
@@ -115,8 +219,30 @@ namespace AM1.VBirdHiyoko
         /// <returns>開始してから現在の状態への変化情報の配列</returns>
         public static HistoryData[] UpdateToLatestArray()
         {
-            Debug.Log("未実装");
-            return null;
+            ReleaseToLatestListToPool();
+
+            HistoryData result = null;
+            for (int i = 0; i < HistoryObjectList.Count; i++)
+            {
+                if (result == null)
+                {
+                    result = GetBlankHistoryData();
+                }
+                if (HistoryObjectList.objectList[i].ToLatestData(ref result))
+                {
+                    // 変更があったので記録
+                    toLatestList.AddLast(result);
+                    result = null;
+                }
+            }
+
+            // 余ったらプールに戻す
+            if (result != null)
+            {
+                ReleaseHistoryDataToPool(result);
+            }
+
+            return ToLatestArray;
         }
 
         /// <summary>
@@ -126,7 +252,15 @@ namespace AM1.VBirdHiyoko
         /// <returns>該当するインデックス。対応するものがなければ-1</returns>
         public static int GetStepIndex(int step)
         {
-            Debug.Log("未実装");
+            int target = step & CounterAnd;
+            for (int i = 0; i < HistoryArray.Length; i++)
+            {
+                if (HistoryArray[i].step == target)
+                {
+                    return i;
+                }
+            }
+
             return -1;
         }
 
